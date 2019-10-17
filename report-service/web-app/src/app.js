94c1dbd32e255c18c313ee14203af01c1e72d33e
potapovDim <potapovd3012@gmail.com>
2019-10-17 18:55:35 +0300

    request added

--- report-service/web-app/src/app.js
+++ report-service/web-app/src/app.js
@@ -4,10 +4,19 @@ import React, {Component} from "react";
 import './styles/app.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      a: 1,
+      b: 2
+    }
+  }
+
   render() {
+    const {a, b} = this.state
     return (
       <div>
-        <h1>Initial React App</h1>
+        <h1>Initial React App {a} {b}</h1>
       </div>
     );
   }
--- report-service/web-app/src/utils/request.js
+++ report-service/web-app/src/utils/request.js
@@ -1,2 +1,22 @@
+// polifill
+import 'whatwg-fetch'
+
+const {ENV} = process.env
+const host = ENV === 'production' ? window.origin : 'http://localhost:3000'
+
+function requestInterface(path, body) {
+  const token = localStorage.getItem('token')
+
+  return fetch(`${host}/${path}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body
+  })
+}
 
 
+export {
+  requestInterface
+}
