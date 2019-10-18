23c3e43e843677c35734e32292df95288a0bc334
potapovDim <potapovd3012@gmail.com>
2019-10-18 18:42:09 +0300

    add mysql to index.sh devops

---
+++ report-service/server/devops/index.sh
@@ -0,0 +1,2 @@
+docker run --restart=always -p 127.0.0.1:27050:27017 --hostname mongo-test --name mongo-test -d mongo --setParameter failIndexKeyTooLong=false \\
+docker run --restart=always --name mysql-test -e MYSQL_ROOT_PASSWORD=report-service-pwd -e MYSQL_USER=report-service-user -d mysql
--- report-service/server/index.js
+++ report-service/server/index.js
@@ -10,7 +10,7 @@ app
   .use(bodyParser())
   .use(router.routes())
 
-if(process.env.NODE_ENV === 'debug') {
+if(process.env.NODE_ENV === 'debug' || process.env.DEBUG) {
   const logger = require('koa-logger')
   app.use(logger())
 }
