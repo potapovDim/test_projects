4f45a2bc407971a0b64244f48d8fe4f961f2822e
potapovDim <potapovd3012@gmail.com>
2019-11-28 12:43:45 +0200

    add for index.sh

--- report-service/server/devops/index.sh
+++ report-service/server/devops/index.sh
@@ -1,2 +1,3 @@
 docker run --restart=always -p 127.0.0.1:27050:27017 --hostname mongo-test --name mongo-test -d mongo --setParameter failIndexKeyTooLong=false \\
 docker run --restart=always --name mysql-test -e MYSQL_ROOT_PASSWORD=report-service-pwd -e MYSQL_USER=report-service-user -d mysql
+docker run --restart=always -d -p 8086:8086 -v $PWD/temp:/var/lib/influxdb influxdb
