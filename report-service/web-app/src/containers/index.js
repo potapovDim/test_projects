a4a705b87037ae5269e5b7cdd9139c9cf0537a2f
potapovDim <potapovd3012@gmail.com>
2019-12-30 10:26:58 +0200

    add containers module

---
+++ report-service/web-app/src/containers/index.js
@@ -0,0 +1,17 @@
+import ReportConfig from './configuration'
+import Header from './header'
+import NavigationMenu from './navigation.menu'
+import RunStatistics from './run.statistics'
+import StatisticsFailedReasons from './statistics.failed.reasons'
+import StatisticsFlakyCases from './statistics.flaky.cases'
+import FailedCasesList from './test.cases.list'
+
+export {
+  ReportConfig,
+  Header,
+  NavigationMenu,
+  RunStatistics,
+  StatisticsFailedReasons,
+  StatisticsFlakyCases,
+  FailedCasesList
+}
